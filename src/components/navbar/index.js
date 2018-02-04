5a675bfe4b5f5b2611009d7e2f2792c811182bc0
Ian <mus5002@aol.com>
2018-02-04 01:28:30 -0500

    navbar styling

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { SocialIcon } from 'react-social-icons'
 import './index.css'
 
 class NavBar extends Component {
   render() {
+    const height = 25;
+    const width = 25;
+
     return (
       <div className="navbar">
-          <h1 className="app-title">Pinterest Clone</h1>
-          <button classname="login-button">Login</button>
+        <SocialIcon className="pinterest-icon" network="pinterest" style={{ height, width }} />
+        <h1 className="app-title">Pinterest Clone</h1>
+        <button className="add-pin-button">ADD</button>
+        <button className="login-button"><SocialIcon network="twitter" style={{ height, width }} /> Login</button>
       </div>
     );
   }
